9964cf346a66a11a4a91f2dbdacf2ae6d00de505
spockula <chinedukogu@gmail.com>
2019-07-16 20:39:29 +0100

    added new fields

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 AUTH_USER_MODEL = 'api.User'
 
